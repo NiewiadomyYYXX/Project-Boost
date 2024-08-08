644d680888f0aeb933ee92002d458f48a96cf574
NiewiadomyYYXX <niewiadomyyyxx49@gmail.com>
2024-08-08 08:20:57 +0200

    App quit

---
+++ Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApplication : MonoBehaviour
+{
+    void Update()
+    {
+        QuitApp();
+    }
+
+    void QuitApp()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Exiting...");
+            Application.Quit();
+        }
+    }
+}
